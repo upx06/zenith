869570a9548d9fd5bde6a327b5823f1886be09f1
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-10-08 21:40:25 -0300

    feat: user queries, mutations and policies

--- lib/zenith/accounts/user.ex
+++ lib/zenith/accounts/user.ex
@@ -4,7 +4,8 @@ defmodule Zenith.Accounts.User do
     domain: Zenith.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshAuthentication]
+    extensions: [AshAuthentication, AshGraphql.Resource],
+    primary_read_warning?: false
 
   authentication do
     add_ons do
@@ -36,42 +37,77 @@ defmodule Zenith.Accounts.User do
     end
   end
 
+  graphql do
+    type :user
+
+    queries do
+      list :list_users, :read
+      read_one :sign_in, :sign_in_with_password, type_name: :user_with_token
+    end
+
+    mutations do
+      create :create_user, :register_with_password
+    end
+  end
+
   postgres do
     table "users"
     repo Zenith.Repo
   end
 
   actions do
-    defaults [:read]
+    action :request_password_reset_token do
+      description "Send password reset instructions to a user if they exist."
 
-    read :get_by_subject do
-      description "Get a user by the subject claim in a JWT"
-      argument :subject, :string, allow_nil?: false
-      get? true
-      prepare AshAuthentication.Preparations.FilterBySubject
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      # creates a reset token and invokes the relevant senders
+      run {AshAuthentication.Strategy.Password.RequestPasswordReset, action: :get_by_email}
     end
 
-    update :change_password do
-      require_atomic? false
-      accept []
-      argument :current_password, :string, sensitive?: true, allow_nil?: false
+    create :register_with_password do
+      description "Register a new user with a email and password."
 
-      argument :password, :string,
-        sensitive?: true,
-        allow_nil?: false,
-        constraints: [min_length: 8]
+      argument :email, :ci_string, allow_nil?: false
+      argument :name, :string, allow_nil?: false
 
-      argument :password_confirmation, :string, sensitive?: true, allow_nil?: false
+      argument :password, :string do
+        description "The proposed password for the user, in plain text."
+        allow_nil? false
+        constraints min_length: 8
+        sensitive? true
+      end
 
-      validate confirm(:password, :password_confirmation)
+      argument :password_confirmation, :string do
+        description "The proposed password for the user (again), in plain text."
+        allow_nil? false
+        sensitive? true
+      end
 
-      validate {AshAuthentication.Strategy.Password.PasswordValidation,
-                strategy_name: :password, password_argument: :current_password}
+      # Sets the email from the argument
+      change set_attribute(:email, arg(:email))
+      change set_attribute(:name, arg(:name))
 
-      change {AshAuthentication.Strategy.Password.HashPasswordChange, strategy_name: :password}
+      # Hashes the provided password
+      change AshAuthentication.Strategy.Password.HashPasswordChange
+
+      # Generates an authentication token for the user
+      change AshAuthentication.GenerateTokenChange
+
+      # validates that the password matches the confirmation
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
+
+      metadata :token, :string do
+        description "A JWT that can be used to authenticate the user."
+        allow_nil? false
+      end
     end
 
     read :sign_in_with_password do
+      graphql(type: :user_with_token, expose_metadata?: true)
+
       description "Attempt to sign in using a email and password."
       get? true
 
@@ -112,46 +148,11 @@ defmodule Zenith.Accounts.User do
       end
     end
 
-    create :register_with_password do
-      description "Register a new user with a email and password."
-
-      argument :email, :ci_string do
-        allow_nil? false
-      end
-
-      argument :password, :string do
-        description "The proposed password for the user, in plain text."
-        allow_nil? false
-        constraints min_length: 8
-        sensitive? true
-      end
-
-      argument :password_confirmation, :string do
-        description "The proposed password for the user (again), in plain text."
-        allow_nil? false
-        sensitive? true
-      end
-
-      change set_attribute(:email, arg(:email))
-      change AshAuthentication.Strategy.Password.HashPasswordChange
-      change AshAuthentication.GenerateTokenChange
-
-      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
-
-      metadata :token, :string do
-        description "A JWT that can be used to authenticate the user."
-        allow_nil? false
-      end
-    end
-
-    action :request_password_reset_token do
-      description "Send password reset instructions to a user if they exist."
-
-      argument :email, :ci_string do
-        allow_nil? false
-      end
-
-      run {AshAuthentication.Strategy.Password.RequestPasswordReset, action: :get_by_email}
+    read :get_by_subject do
+      description "Get a user by the subject claim in a JWT"
+      argument :subject, :string, allow_nil?: false
+      get? true
+      prepare AshAuthentication.Preparations.FilterBySubject
     end
 
     read :get_by_email do
@@ -163,6 +164,26 @@ defmodule Zenith.Accounts.User do
       end
 
       filter expr(email == ^arg(:email))
+    end
+
+    update :change_password do
+      require_atomic? false
+      accept []
+      argument :current_password, :string, sensitive?: true, allow_nil?: false
+
+      argument :password, :string,
+        sensitive?: true,
+        allow_nil?: false,
+        constraints: [min_length: 8]
+
+      argument :password_confirmation, :string, sensitive?: true, allow_nil?: false
+
+      validate confirm(:password, :password_confirmation)
+
+      validate {AshAuthentication.Strategy.Password.PasswordValidation,
+                strategy_name: :password, password_argument: :current_password}
+
+      change {AshAuthentication.Strategy.Password.HashPasswordChange, strategy_name: :password}
     end
 
     update :reset_password_with_token do
@@ -192,10 +213,38 @@ defmodule Zenith.Accounts.User do
 
       change AshAuthentication.GenerateTokenChange
     end
+
+    # update :change_active_value do
+    #   require_atomic? false
+
+    #   change fn changeset, _ ->
+    #     value = Ash.Changeset.get_attribute(changeset, :active)
+    #     Ash.Changeset.change_attribute(changeset, :active, not value)
+    #   end
+    # end
+
+    read :read do
+      primary? true
+      prepare build(sort: [created_at: :desc])
+    end
+
+    read :read_paginated do
+      pagination do
+        required? false
+        keyset? true
+        default_limit 15
+        countable true
+        max_page_size 15
+      end
+    end
   end
 
   policies do
     bypass AshAuthentication.Checks.AshAuthenticationInteraction do
+      authorize_if always()
+    end
+
+    policy always() do
       authorize_if always()
     end
   end
