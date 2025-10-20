d7422d9ebb7b8fdad31211388876d555baeade0e
GUSGOMESZ <gomesgustavo1504@gmail.com>
2025-10-20 13:00:33 -0300

    feat: adding exaws config

--- config/config.exs
+++ config/config.exs
@@ -105,6 +105,10 @@ config :logger, :default_formatter,
 config :phoenix, :json_library, Jason
 
 # AWS Pessoal
+config :ex_aws,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
