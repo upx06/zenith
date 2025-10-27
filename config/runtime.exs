79ee1978726c7e5008cc22d5c727767f6eeb0e3b
GUSGOMESZ <gomesgustavo1504@gmail.com>
2025-10-27 12:17:55 -0300

    fix: enable server in production

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,6 +56,7 @@ if config_env() == :prod do
   config :zenith, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :zenith, ZenithWeb.Endpoint,
+    server: true,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
