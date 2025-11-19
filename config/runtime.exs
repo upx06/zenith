ffa93d815b992e328cde1dde984ccf7049ef5521
GUSGOMESZ <gomesgustavo1504@gmail.com>
2025-11-19 13:52:33 -0300

    feat: chromium exe on runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -79,6 +79,7 @@ if config_env() == :prod do
     region: System.get_env("AWS_REGION")
 
   config :chromic_pdf,
+    chrome_executable: "/usr/bin/chromium",
     chrome_args: [
       "--no-sandbox",
       "--disable-dev-shm-usage",
